37da380fcf19c2f02f67275bb6f5dd7f105f2544
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2023-06-02 08:40:52 -0500

    Update dataadding.sql

--- database/dataadding.sql
+++ database/dataadding.sql
@@ -1,2 +1,2 @@
 SELECT * FROM testing.employees;
-INSERT INTO `testing`.`employees` (`first_name`, `last_name`, `email`, `phone`, `organization`, `designation`, `salary`, `status`, `is_deleted`, `created_at`) VALUES ('Rabiyulfahim', 'H', 'rabiyulfahimh@gmail.com', '8778763117', 'Openai', 'Python Developer', '500.00', '1', '0', '2023-05-23 03:35:30');
+INSERT INTO `testing`.`employees` (`first_name`, `last_name`, `email`, `phone`, `organization`, `designation`, `salary`, `status`, `is_deleted`, `created_at`) VALUES ('Rabiyulfahim', 'H', 'rabiyul@gmail.com', '8778763117', 'Openai', 'Python Developer', '500.00', '1', '0', '2023-05-23 03:35:30');
